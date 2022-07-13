c26dcadd56bf7ad9a35969c066646d336fa2e747
Yonah Karp <yonah.karp@aexp.com>
2022-07-13 11:45:17 -0400

    Updates

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -30507,7 +30507,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55790" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55126" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
